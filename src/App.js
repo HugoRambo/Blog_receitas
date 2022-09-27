fb5f31a3ac6a174459bf894e7af5e55e65ca8d6a
Hugo Rambo <hugo.rambo@hotmail.com>
2022-09-27 10:27:14 -0300

    melhorando funcionaçodade do login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@
 import './App.css';
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 
+//Função que mapeia se a autenticação do usuário foi feita com sucesso
+import { onAuthStateChanged } from 'firebase/auth';
+//Vou importar o hook que eu criei para validação 
+import { useAuthentication } from './hooks/useAuthentication';
+
+//hooks
+import {  useState, useEffect} from "react"
+
 //context
 import {  AuthProvider }from "./context/AuthContenxt" 
 
@@ -16,9 +24,32 @@ import Register from './pages/Register/Register';
 
 
 function App() {
+  //Começo sem usuário.
+  const [user, setUser] =useState(undefined)
+  
+  //Invoco meu hook de autenticação
+  const {auth} = useAuthentication()
+  
+  const loadingUser = user === undefined 
+  
+  //Sempre que mudar a autenticação vai ser mudado nosso useEffect
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) =>{
+      setUser(user)
+    })
+    //Aqui passo o usuário que quero colocar na variavel setuser
+
+  }, [auth])
+  
+  
+  //Se esta carregando usuário eu 
+  if(loadingUser){
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className="App">
-      <AuthProvider>
+      <AuthProvider value={{user}}>
       <BrowserRouter>
       <Navbar/>
         <div className='container'>
